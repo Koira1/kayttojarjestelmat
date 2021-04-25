b7a0a278e012e39ef24041dd9423dde798a2ea6b
Semi Rantanen <semi.rantanen1@hotmail.com>
2021-04-25 14:06:04 -0700

    xv6 delta

---
+++ XV6-changes/cat.c
@@ -0,0 +1,49 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+char buf[512];
+
+void
+cat(int fd)
+{
+  int n;
+
+  while((n = read(fd, buf, sizeof(buf))) > 0) {
+    if (write(1, buf, n) != n) {
+      printf(1, "cat: write error\n");
+      exit();
+    }
+  }
+  if(n < 0){
+    printf(1, "cat: read error\n");
+    exit();
+  }
+}
+
+int
+main(int argc, char *argv[])
+{
+  int fd, i;
+  int count1 = readcount();
+
+
+  if(argc <= 1){
+    cat(0);
+    exit();
+  }
+
+  for(i = 1; i < argc; i++){
+    if((fd = open(argv[i], 0)) < 0){
+      printf(1, "cat: cannot open %s\n", argv[i]);
+      exit();
+    }
+    cat(fd);
+    int count = readcount();
+    printf(1, "\n");
+    printf(1, "Read was called in the beginning: %d times\n", count1);
+    printf(1, "Read has been called: %d times\n", count);
+    close(fd);
+  }
+  exit();
+}
